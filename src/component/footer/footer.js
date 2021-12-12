b28538767055a73850fff6bddc6deca1c14ed57e
ibn2085 <yahyahridwan665@gmail.com>
2021-12-12 17:15:48 +0545

    adding responsiveness

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -7,9 +7,9 @@ export default function Footer(){
         <div class="footer">
             <p>{(new Date().getFullYear())}. All Right Reserve</p>
                 <ul>
-                    <li><Link to="/">About us</Link></li>
-                    <li><Link to="/">Business</Link></li>
-                    <li><Link to="/">Personal</Link></li>
+                    <li><Link to="/"><i class="fab fa-facebook"></i></Link></li>
+                    <li><Link to="/"><i class="fas fa-envelope-open-text"></i></Link></li>
+                    <li><Link to="/"><i class="fab fa-twitter"></i></Link></li>
                 </ul>
                 <div class="row">
                     <p>Terms & Condition</p>
