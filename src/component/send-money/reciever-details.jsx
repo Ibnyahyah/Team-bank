0d5105bb08c525cc89b821194a75dee58dc702c8
ibn2085 <yahyahridwan665@gmail.com>
2021-12-13 23:58:30 +0545

    making some correction

--- src/component/send-money/reciever-details.jsx
+++ src/component/send-money/reciever-details.jsx
@@ -40,17 +40,17 @@ export default function ReceiverDetail(props){
     } 
 
     return(
-        <div className="container mt-2 display-f align-center justify-center verifer">
+        <div className="mt-2 display-f align-center justify-center verifer">
             <div className="card sending-card mt-5">
                 <div className="mt-3 mb-2">
-                    <h3 className="font-4 font-md">Receiver's Name</h3>
-                    <p className="font-3 font-md">{data.name}</p>
-                    <h3 className="font-4 font-md">Receiver's account number</h3>
-                    <p className="font-3 font-md">{data.accountNumber}</p>
-                    <h3 className="font-4 font-md">Receiver's Username</h3>
-                    <p className="font-3 font-md">{data.userName}</p>
-                    <h3 className="font-4 font-md">Receiver's Email address</h3>
-                    <p className="font-3 font-md">{data.email}</p>
+                    <h3 className="font-4 font-md">Receiver's Name :</h3>
+                    <p className="font-2 font-md text-blue mt-1">{data.name}</p>
+                    <h3 className="font-4 font-md">Receiver's account number :</h3>
+                    <p className="font-2 font-md text-blue mt-1">{data.accountNumber}</p>
+                    <h3 className="font-4 font-md">Receiver's Username :</h3>
+                    <p className="font-2 font-md text-blue mt-1">{data.userName}</p>
+                    <h3 className="font-4 font-md">Receiver's Email address :</h3>
+                    <p className="font-2 font-md text-blue mt-1">{data.email}</p>
 
                 </div>
                 <form onSubmit={handleSubmit}>
