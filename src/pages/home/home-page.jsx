55f13951a4c3e94871ee3c81cb3571ec53861ee5
ibn2085 <yahyahridwan665@gmail.com>
2021-12-13 07:49:48 +0545

    adding homepage

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Navbar from "../../component/navbar/navbar";
 
 export default function Home(){
@@ -11,9 +12,20 @@ export default function Home(){
                     <div>
                         <p>Stay in control of your finance with Team-Bank</p>
                     </div>
+                <button><Link to="/login" style={{color:'#133F77'}}>Get Started</Link></button>
                 </div>
             </div>
         </div>
+            <div className="container" style={{marginTop:'100px', background:'#133f77', color:'white'}}>
+                <div className="row">
+                    <div className="card">
+                        <p>Make Deposit</p>
+                    </div>
+                    <div className="card">Withdraw</div>
+                    <div className="card">Transfer</div>
+                    <div className="card">Online Payment</div>
+                </div>
+            </div>
         </>
     )
 }
