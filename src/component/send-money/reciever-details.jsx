f23051933a0c3fffb1d6f23273269d39d63ce2f5
SychWalley <adedejiyusuf26@gmail.com>
2021-12-13 23:02:33 +0100

    Transaction

--- src/component/send-money/reciever-details.jsx
+++ src/component/send-money/reciever-details.jsx
@@ -1,7 +1,7 @@
 // import { Link } from "react-router-dom";
 import React, { useState } from 'react'
 // import { Link } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { transferUsingAccountNumber } from '../../actions/transaction'
 
 
